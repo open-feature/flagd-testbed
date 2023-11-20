821c5deb0938ed5bef53e046349a5806d53ee4a0
Todd Baert <todd.baert@dynatrace.com>
2023-11-20 09:43:36 -0500

    feat: add unstable testbeds (#77)
    Signed-off-by: Todd Baert <todd.baert@dynatrace.com>

---
+++ scripts/restart-wrapper.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# wrapper script to start flagd/sync, and restart it every 5 seconds (to test reconnect functionality)
+
+killed=0
+
+# handle SIGINTs and SIGTERMs so we can kill the container
+handle_term() { 
+  kill -TERM "$child" 2>/dev/null
+  killed=1
+}
+
+handle_int() { 
+  kill -INT "$child" 2>/dev/null
+  killed=1
+}
+
+trap handle_term SIGTERM
+trap handle_int SIGINT
+
+while [ "$killed" -eq 0 ]; # stop looping if we were interrupted
+do
+    # start change script and our server
+    echo 'starting process...' 
+    "$@" &
+    child=$!
+    sleep 5
+    echo "killing pid $child..." && sleep 5 && kill -9 "$child"
+done
